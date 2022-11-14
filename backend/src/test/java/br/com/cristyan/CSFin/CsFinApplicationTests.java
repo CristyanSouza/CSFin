127a9ff0ca56a7618975225266f05aa6d2cd628b
Cristyan de Souza <cristyansouzas2@gmail.com>
2022-11-14 07:40:13 -0300

    Projeto criado

---
+++ backend/src/test/java/br/com/cristyan/CSFin/CsFinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cristyan.CSFin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
