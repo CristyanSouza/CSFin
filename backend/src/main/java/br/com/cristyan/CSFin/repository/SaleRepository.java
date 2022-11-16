e8b48a49c9534486911343eac72ad5f5f0faba92
Cristyan de Souza <cristyansouzas2@gmail.com>
2022-11-16 08:03:46 -0300

    Consulta por data e peageble

--- backend/src/main/java/br/com/cristyan/CSFin/repository/SaleRepository.java
+++ backend/src/main/java/br/com/cristyan/CSFin/repository/SaleRepository.java
@@ -1,6 +1,11 @@
 package br.com.cristyan.CSFin.repository;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.cristyan.CSFin.model.Sale;
@@ -8,5 +13,6 @@ import br.com.cristyan.CSFin.model.Sale;
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
-	
+	@Query("SELECT s FROM tb_sales s WHERE s.date BETWEEN :min AND :max ORDER BY s.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
