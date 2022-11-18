c2be3ccd3504f85e9dbba082b645c8b6c888f55b
Cristyan de Souza <cristyansouzas2@gmail.com>
2022-11-18 07:01:45 -0300

    Axios e useEffect

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,8 @@ import NotificationButton from "../NotificationButton";
 import DatePicker from "react-datepicker";
 import "./styles.css";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 
 function SalesCard() {
@@ -12,6 +13,13 @@ function SalesCard() {
     
     const [dataMin, setDataMin] = useState(min);
     const [dataMax, setDataMax] = useState(max);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/sale")
+        .then(response => {
+            console.log(response.data)
+        })
+    }, []);
 
 
     return (
