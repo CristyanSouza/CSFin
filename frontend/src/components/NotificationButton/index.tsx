d439619ec79d3eb914eac62a3f510b5639ca4f8b
Cristyan de Souza <cristyansouzas2@gmail.com>
2022-11-18 08:11:37 -0300

    Notificação

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,29 @@
 import icon from "../../assets/img/notification-icon.svg"
+import axios from "axios"
+
 
 import "./styles.css"
+import { BASE_URL } from "../util/util";
 
-function NotificationButton() {
+
+type Props = {
+    saleId: number;
+}
+
+function handleClick (id :Props) {
+    axios.get(`${BASE_URL}/${id.saleId}/notification`)
+        .then(response => {
+            console.log(response)
+        });
+}
+
+function NotificationButton(saleId : Props) {
+
+
+
+
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)} >
             <img src={icon} alt="Notificar" />
         </div>
     );
